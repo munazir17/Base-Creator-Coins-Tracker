8dfeb9563fed15fc202f40aafa3d9a4f9c4b3e54
munazir Raza <139262123+munazir17@users.noreply.github.com>
2025-10-20 13:35:50 +0530

    Add service worker for caching assets

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+// public/sw.js
+const CACHE_NAME = 'base-creator-coins-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logos/default.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});

f54c5a1d23ebbec9e0b2bb9cd8e37ae4710442cf
munazir Raza <139262123+munazir17@users.noreply.github.com>
2025-10-20 12:10:23 +0530

    Update TALENT_API_KEY environment variable

--- api/talent.js
+++ api/talent.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   
-  const TALENT_API_KEY = process.env.4b362f11cf8b0f76d6d9bfecea213067f9c98d5e97b3cc82c17607c14098;
+  const TALENT_API_KEY = process.env.cb4dfbcc3d0cea99283eee59ecbefb30cce177f4acdcef61db668e5030c2;
   const { username } = req.query;
   
   try {
